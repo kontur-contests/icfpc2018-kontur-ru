96c0607bdc8d18b65facde03cb49f6108818066e
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2018-07-15 16:30:33 +0500

    Add Elastic connection example

--- houston-runner/HoustonRunner.cs
+++ houston-runner/HoustonRunner.cs
@@ -1,16 +1,36 @@
+using System;
 using System.Threading;
 
 using Kontur.Houston.Plugin;
 
+using Nest;
+
 public class HoustonRunner : IPlugin<HoustonRunnerProperties>
 {
+    class Message
+    {
+        public string Text;
+    }
+    
     public void Run(IPluginContext<HoustonRunnerProperties> context)
     {
+        var settings = new ConnectionSettings(new Uri("http://efk2-elasticsearch9200.efk2.10.217.14.7.xip.io"))
+            .DefaultIndex("test");
+
+        var client = new ElasticClient(settings);
+        
         while (!context.CancellationToken.IsCancellationRequested)
         {
+            // using houston-provided properties
             context.Log.Info(context.Properties.message);
+
+            Thread.Sleep(10000);
+           
+            // using elastic response
+            var response = client.Get<Message>("1");
+            context.Log.Info(response.Source.Text);
  
-            Thread.Sleep(1000);
+            Thread.Sleep(10000);
         }
  
         context.Log.Info("Bye!");
