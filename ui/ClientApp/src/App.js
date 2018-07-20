4e112ff7698ef5e38129e0865af96bfa411f71b8
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-07-21 02:14:31 +0500

    chore: requesting data from server

--- ui/ClientApp/src/App.js
+++ ui/ClientApp/src/App.js
@@ -30,30 +30,58 @@ const steps = [
 
 export default class App extends Component {
   state = {
-    step: 0
+    step: 0,
+    modelId: 0,
+    steps: []
   };
 
+  componentDidMount() {
+    this.requestModel(0);
+  }
+
   render() {
-    const stepData = steps[this.state.step];
+    const stepData = this.state.steps[this.state.step];
     return (
       <div style={{ maxWidth: "800px", margin: "auto" }}>
-        <Visualizer model={stepData.model} bots={stepData.bots} />
-        <hr />
         <input
-          style={{ width: "100%" }}
-          type="range"
-          onChange={this.handleStepChange}
-          min={0}
-          value={this.state.step}
-          max={steps.length - 1}
+          type="number"
+          value={this.state.modelId}
+          onChange={this.handleModelChange}
         />
-        <center>Step: {this.state.step}</center>
+        {stepData && (
+          <React.Fragment>
+            <Visualizer model={stepData.model} bots={stepData.bots} />
+            <hr />
+            <input
+              style={{ width: "100%" }}
+              type="range"
+              onChange={this.handleStepChange}
+              min={0}
+              value={this.state.step}
+              max={steps.length - 1}
+            />
+            <center>Step: {this.state.step}</center>
+          </React.Fragment>
+        )}
       </div>
     );
   }
 
+  requestModel = id => {
+    fetch(`/api/matrix/index?i=${id}`)
+      .then(x => x.json())
+      .then(model => {
+        this.setState({ steps: [{ model, bots: [] }], step: 0 });
+      });
+  };
+
   handleStepChange = event => {
     const step = parseInt(event.target.value);
     this.setState({ step });
+  };
+
+  handleModelChange = event => {
+    const modelId = parseInt(event.target.value);
+    this.setState({ modelId, steps: [] }, () => this.requestModel(modelId));
   };
 }
--- ui/Controllers/ModelController.cs
+++ ui/Controllers/ModelController.cs
@@ -9,34 +9,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ui.Controllers
 {
-    [Route("api/[controller]")]
-    public class MatrixController : Controller
+  [Route("api/[controller]")]
+  public class MatrixController : Controller
+  {
+    [HttpGet("[action]")]
+    public int[,,] Index(int i)
     {
-        [HttpGet("[action]")]
-        public int[,,] Index(int i)
+      const string problemsDir = "../data/problemsL";
+      var filename = Directory.EnumerateFiles(problemsDir, "*.mdl").ToList()[i];
+
+      if (filename == null) return null;
+
+      var content = System.IO.File.ReadAllBytes(filename);
+
+      var voxels = Matrix.Load(content).Voxels;
+      var ints = new int[voxels.GetLength(0), voxels.GetLength(1), voxels.GetLength(2)];
+
+      for (var x = 0; x < voxels.GetLength(0); x++)
+      {
+        for (var y = 0; y < voxels.GetLength(1); y++)
         {
-            const string problemsDir = "../data/problemsL";
-            var filename = Directory.EnumerateFiles(problemsDir, "*.mdl").ToList()[i];
-
-            if (filename == null) return null;
-            
-            var content = System.IO.File.ReadAllBytes(filename);
-
-            var voxels = Matrix.Load(content).Voxels;
-            var ints = new int[voxels.GetLength(0),voxels.GetLength(1),voxels.GetLength(2)];
-
-            for (var x = 0; x < voxels.GetLength(0); x++)
-            {
-                for (var y = 0; x < voxels.GetLength(1); x++)
-                {
-                    for (var z = 0; x < voxels.GetLength(2); x++)
-                    {
-                        ints[x, y, z] = voxels[x, y, z] ? 1 : 0;
-                    }
-                }
-            }
-
-            return ints;
+          for (var z = 0; z < voxels.GetLength(2); z++)
+          {
+            ints[x, y, z] = voxels[x, y, z] ? 1 : 0;
+          }
         }
+      }
+
+      return ints;
     }
+  }
 }
