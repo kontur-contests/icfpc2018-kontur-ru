5a0a7b09580e589899870436994f1c4c87ea5da0
Igor Lukanin <mail@igor.lukanin.name>
2018-07-21 02:16:23 +0500

    Strings

--- ui/Controllers/ModelController.cs
+++ ui/Controllers/ModelController.cs
@@ -3,40 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+
 using lib.Models;
 
 using Microsoft.AspNetCore.Mvc;
 
 namespace ui.Controllers
 {
-  [Route("api/[controller]")]
-  public class MatrixController : Controller
-  {
-    [HttpGet("[action]")]
-    public int[,,] Index(int i)
+    [Route("api/[controller]")]
+    public class MatrixController : Controller
     {
-      const string problemsDir = "../data/problemsL";
-      var filename = Directory.EnumerateFiles(problemsDir, "*.mdl").ToList()[i];
-
-      if (filename == null) return null;
-
-      var content = System.IO.File.ReadAllBytes(filename);
-
-      var voxels = Matrix.Load(content).Voxels;
-      var ints = new int[voxels.GetLength(0), voxels.GetLength(1), voxels.GetLength(2)];
-
-      for (var x = 0; x < voxels.GetLength(0); x++)
-      {
-        for (var y = 0; y < voxels.GetLength(1); y++)
+        [HttpGet("[action]")]
+        public string[,,] Index(int i)
         {
-          for (var z = 0; z < voxels.GetLength(2); z++)
-          {
-            ints[x, y, z] = voxels[x, y, z] ? 1 : 0;
-          }
-        }
-      }
+            const string problemsDir = "../data/problemsL";
+            var filename = Directory.EnumerateFiles(problemsDir, "*.mdl").ToList()[i];
 
-      return ints;
+            if (filename == null) return null;
+
+            var content = System.IO.File.ReadAllBytes(filename);
+
+            var voxels = Matrix.Load(content).Voxels;
+            var strings = new string[voxels.GetLength(0), voxels.GetLength(1), voxels.GetLength(2)];
+
+            for (var x = 0; x < voxels.GetLength(0); x++)
+            {
+                for (var y = 0; y < voxels.GetLength(1); y++)
+                {
+                    for (var z = 0; z < voxels.GetLength(2); z++)
+                    {
+                        strings[x, y, z] = voxels[x, y, z] ? "" : "0";
+                    }
+                }
+            }
+
+            return strings;
+        }
     }
-  }
 }
