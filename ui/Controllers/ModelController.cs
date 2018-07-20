e5caefe337c291c0e1c3a463e2bceec2439ed37d
Igor Lukanin <mail@igor.lukanin.name>
2018-07-21 02:08:16 +0500

    Bools to ints

--- ui/Controllers/ModelController.cs
+++ ui/Controllers/ModelController.cs
@@ -13,7 +13,7 @@ namespace ui.Controllers
     public class MatrixController : Controller
     {
         [HttpGet("[action]")]
-        public bool[,,] Index(int i)
+        public int[,,] Index(int i)
         {
             const string problemsDir = "../data/problemsL";
             var filename = Directory.EnumerateFiles(problemsDir, "*.mdl").ToList()[i];
@@ -21,7 +21,22 @@ namespace ui.Controllers
             if (filename == null) return null;
             
             var content = System.IO.File.ReadAllBytes(filename);
-            return Matrix.Load(content).Voxels;
+
+            var voxels = Matrix.Load(content).Voxels;
+            var ints = new int[voxels.GetLength(0),voxels.GetLength(1),voxels.GetLength(2)];
+
+            for (var x = 0; x < voxels.GetLength(0); x++)
+            {
+                for (var y = 0; x < voxels.GetLength(1); x++)
+                {
+                    for (var z = 0; x < voxels.GetLength(2); x++)
+                    {
+                        ints[x, y, z] = voxels[x, y, z] ? 1 : 0;
+                    }
+                }
+            }
+
+            return ints;
         }
     }
 }
