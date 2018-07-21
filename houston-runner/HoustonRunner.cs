3ea47f4952288cff2e386817dfc303ee87dbb6e5
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2018-07-21 13:20:36 +0500

    Intial Houston-based solver runner

--- houston-runner/HoustonRunner.cs
+++ houston-runner/HoustonRunner.cs
@@ -1,9 +1,14 @@
 using System;
+using System.IO;
 using System.Threading;
 
 using Kontur.Houston.Plugin;
 
 using lib;
+using lib.ElasticDTO;
+using lib.Models;
+using lib.Strategies;
+using lib.Utils;
 
 using Nest;
 
@@ -13,26 +18,40 @@ namespace houston
     {
         public void Run(IPluginContext<HoustonRunnerProperties> context)
         {
-            var client = new ElasticClient(new ConnectionSettings(new Uri("http://efk2-elasticsearch9200.efk2.10.217.14.7.xip.io")).DefaultIndex("test2"));
+            var client = new ElasticClient(new ConnectionSettings(new Uri("http://efk2-elasticsearch9200.efk2.10.217.14.7.xip.io")).DefaultIndex("testruns"));
 
             while (!context.CancellationToken.IsCancellationRequested)
             {
-                // using houston-provided properties
-                context.Log.Info(context.Properties.message);
+                var problemId = 1;
+                var problemsDir = Path.Combine(Environment.CurrentDirectory, "../data/problemsL");
+                var problemFileName = $"LA{problemId.ToString().PadLeft(3, '0')}_tgt.mdl";
+                var matrix = Matrix.Load(File.ReadAllBytes(Path.Combine(problemsDir, problemFileName)));
+                var R = matrix.R;
 
-                Log.For(this).Info("Hello via log4stash");
+                var solver = new GreedyPartialSolver(matrix.Voxels, new bool[R, R, R], new Vec(0, 0, 0), new ThrowableHelper(matrix));
+                var testResult = new TaskRunMeta
+                    {
+                        RunningHostName = Environment.MachineName,
+                        SolverName = solver.GetType().Name,
+                        StartedAt = DateTime.Now,
+                        TaskName = problemFileName,
+                    };
+                context.Log.Info($"Starting task {testResult.TaskName} at {testResult.RunningHostName} with solver {testResult.SolverName}");
 
-                Thread.Sleep(10000);
-
-                // using elastic response
-                var response = client.Get<TestMessage>("1");
-                if (response.Source != null)
-                    context.Log.Info(response.Source.Text);
-                else
+                try
                 {
-                    context.Log.Info("Failed to fetch message from Elastic");
-                    context.Log.Info(response.DebugInformation);
+                    solver.Solve();
+
+                    //TODO fill IsSuccess, EnergySpent
+
+                    testResult.Solution = Convert.ToBase64String(CommandSerializer.Save(solver.Commands.ToArray()));
                 }
+                catch (Exception e)
+                {
+                    context.Log.Warn($"Unhandled exception in solver for {problemFileName}", e);
+                }
+
+                client.IndexDocument(testResult);
 
                 Thread.Sleep(10000);
             }
---
+++ lib/ElasticDTO/TaskRunMeta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Nest;
+
+using Newtonsoft.Json;
+
+namespace lib.ElasticDTO
+{
+    public class TaskRunMeta
+    {
+        public DateTime StartedAt;
+        public int SecondsSpent;
+        public long EnergySpent;
+        public string TaskName;
+        public string SolverName;
+        public string RunningHostName;
+        public bool IsSuccess;
+
+        [Binary(Store = false)]
+        public string Solution { get; set; }
+    }
+}
