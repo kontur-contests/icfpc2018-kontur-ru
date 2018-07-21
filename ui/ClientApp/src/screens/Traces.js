e1046d0d32854fe02c329e9d821ef6323849c35f
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-07-21 17:19:34 +0500

    fix(Traces): fixed alerting errors on index page

--- ui/ClientApp/src/screens/Traces.js
+++ ui/ClientApp/src/screens/Traces.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Visualizer } from "./modules/visualizer";
-import { Plot2D } from "./modules/plot-2d/Plot2D";
+import { Visualizer } from "../modules/visualizer";
+import { Plot2D } from "../modules/plot-2d/Plot2D";
 
 export default class App extends Component {
   state = {
@@ -8,7 +8,8 @@ export default class App extends Component {
     modelId: 0,
     steps: [],
     solutions: [],
-    energy: []
+    energy: [],
+    index: false
   };
 
   componentDidMount() {
@@ -20,21 +21,25 @@ export default class App extends Component {
 
     return (
       <div style={{ maxWidth: "1200px", margin: "auto" }}>
-        {!stepData && <h1>Loading...</h1>}
-        {stepData && <Visualizer model={stepData.model} bots={stepData.bots} />}
-        <hr />
-        <input
-          style={{ width: "100%" }}
-          type="range"
-          onChange={this.handleStepChange}
-          min={0}
-          value={this.state.step}
-          max={this.state.steps.length - 1}
-        />
-        <center>Step: {this.state.step}</center>
-        <hr />
-        <h2>Energy</h2>
-        <Plot2D data={this.state.energy} />
+        {!this.state.index && !stepData && <h2>Loading...</h2>}
+        {stepData && (
+          <React.Fragment>
+            <Visualizer model={stepData.model} bots={stepData.bots} />
+            <hr />
+            <input
+              style={{ width: "100%" }}
+              type="range"
+              onChange={this.handleStepChange}
+              min={0}
+              value={this.state.step}
+              max={this.state.steps.length - 1}
+            />
+            <center>Step: {this.state.step}</center>
+            <hr />
+            <h2>Energy</h2>
+            <Plot2D data={this.state.energy} />
+          </React.Fragment>
+        )}
         <hr />
         <h2>Solutions</h2>
         <ul>
@@ -51,35 +56,24 @@ export default class App extends Component {
   requestMatrix007 = () => {
     const search = document.location.search;
     if (!search.includes("file")) {
-      alert('Specify parameter "file" in url query');
+      this.setState({ index: true });
+    } else {
+      fetch(`/api/matrix/trace${search}`)
+        .then(x => x.json())
+        .then(processData)
+        .then(x =>
+          this.setState({ steps: x.steps, step: 0, energy: x.energy })
+        );
     }
-
-    fetch(`/api/matrix/trace${search}`)
-      .then(x => x.json())
-      .then(processData)
-      .then(x => this.setState({ steps: x.steps, step: 0, energy: x.energy }));
 
     fetch(`/api/matrix/solutions`)
       .then(x => x.json())
       .then(x => this.setState({ solutions: x }));
   };
 
-  requestModel = id => {
-    fetch(`/api/matrix/index?i=${id}`)
-      .then(x => x.json())
-      .then(model => {
-        this.setState({ steps: [{ model, bots: [] }], step: 0 });
-      });
-  };
-
   handleStepChange = event => {
     const step = parseInt(event.target.value);
     this.setState({ step });
-  };
-
-  handleModelChange = event => {
-    const modelId = parseInt(event.target.value);
-    this.setState({ modelId, steps: [] }, () => this.requestModel(modelId));
   };
 }
 
