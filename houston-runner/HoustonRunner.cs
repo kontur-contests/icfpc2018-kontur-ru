71ee883985b6dd39b5f71060a924e0761eda7f47
spaceorc <spaceorc@skbkontur.ru>
2018-07-21 18:11:21 +0500

    Delete trash

--- houston-runner/HoustonRunner.cs
+++ houston-runner/HoustonRunner.cs
@@ -22,10 +22,9 @@ namespace houston
     {
         public void Run(IPluginContext<HoustonRunnerProperties> context)
         {
-            
             const string elasticUrl = "http://efk2-elasticsearch9200.efk2.10.217.14.7.xip.io";
             const string elasticIndex = "testruns";
-            
+
             var replicaNumber = context.Info.Replica.ReplicaNumber;
             var replicaCount = context.Info.Replica.ReplicationFactor;
 
@@ -34,7 +33,7 @@ namespace houston
                 replicaNumber = 1;
                 replicaCount = 1;
             }
-            
+
             context.Log.Info($"Replica # {replicaNumber} of {replicaCount}: preparing...");
 
             var client = new ElasticClient(new ConnectionSettings(new Uri(elasticUrl)).DefaultIndex(elasticIndex));
@@ -42,120 +41,117 @@ namespace houston
             var tasks = ProblemSolutionFactory.GetTasks();
 
             var selectedTasks = tasks
-                .Where(task => ((uint) (task.Problem.Name + task.Solution.Name).GetHashCode()) % replicaCount == replicaNumber - 1)
+                .Where(task => ((uint)(task.Problem.Name + task.Solution.Name).GetHashCode()) % replicaCount == replicaNumber - 1)
                 .ToArray();
 
             context.Log.Info($"Replica # {replicaNumber} of {replicaCount}: " +
                              $"running {selectedTasks.Length} of {tasks.Length} tasks");
 
             var completeTasksCounter = 0;
-            while (!context.CancellationToken.IsCancellationRequested)
-            {
-                selectedTasks.ForEach(task =>
+            selectedTasks.ForEach(task =>
+                {
+                    var solution = task.Solution;
+
+                    var result = new TaskRunMeta
+                        {
+                            StartedAt = DateTime.UtcNow,
+                            TaskName = task.Problem.Name,
+                            SolverName = solution.Name,
+                            RunningHostName = Environment.MachineName
+                        };
+
+                    context.Log.Info($"Task {result.TaskName} " +
+                                     $"with solver {result.SolverName} " +
+                                     $"at {result.RunningHostName}: " +
+                                     $"starting...");
+
+                    try
                     {
-                        var solution = task.Solution;
-                        
-                        var result = new TaskRunMeta
+                        var timer = Stopwatch.StartNew();
+
+                        var solver = solution.Solver();
+
+                        var commands = new List<ICommand>();
+                        var started = new ManualResetEvent(false);
+                        ExceptionDispatchInfo exceptionDispatchInfo = null;
+                        var total = task.Problem.Matrix.Weight;
+                        int done = 0;
+                        var timeout = Stopwatch.StartNew();
+                        var runThread = new Thread(() =>
                             {
-                                StartedAt = DateTime.UtcNow,
-                                TaskName = task.Problem.Name,
-                                SolverName = solution.Name,
-                                RunningHostName = Environment.MachineName
-                            };
-
-                        context.Log.Info($"Task {result.TaskName} " +
-                                         $"with solver {result.SolverName} " +
-                                         $"at {result.RunningHostName}: " +
-                                         $"starting...");
-
-                        try
-                        {
-                            var timer = Stopwatch.StartNew();
-
-                            var solver = solution.Solver();
-
-                            var commands = new List<ICommand>();
-                            var started = new ManualResetEvent(false);
-                            ExceptionDispatchInfo exceptionDispatchInfo = null;
-                            var total = task.Problem.Matrix.Weight;
-                            int done = 0;
-                            var timeout = Stopwatch.StartNew();
-                            var runThread = new Thread(() =>
+                                try
                                 {
-                                    try
+                                    var solverCommands = solver.Solve();
+                                    foreach (var command in solverCommands)
                                     {
-                                        var solverCommands = solver.Solve();
-                                        foreach (var command in solverCommands)
-                                        {
-                                            started.Set();
-                                            commands.Add(command);
-                                            if (command is Fill)
-                                                Interlocked.Increment(ref done);
-                                        }
-                                    }
-                                    catch (Exception exception)
-                                    {
-                                        exceptionDispatchInfo = ExceptionDispatchInfo.Capture(exception);
                                         started.Set();
+                                        commands.Add(command);
+                                        if (command is Fill)
+                                            Interlocked.Increment(ref done);
                                     }
-                                });
-                            runThread.Start();
-                            if (!started.WaitOne(context.Properties.SolverStartTimeout))
-                            {
-                                runThread.Abort();
-                                runThread.Join();
-                                throw new TimeoutException("Solve start timeout expired");
-                            }
-
-                            while (!runThread.Join(context.Properties.SolverTimeoutMeasureInterval))
-                            {
-                                var localDone = Interlocked.CompareExchange(ref done, 0, 0);
-                                if (localDone == 0)
-                                    throw new TimeoutException("Solver didn't fill any cells");
-
-                                var estimatedTotalTime = TimeSpan.FromTicks(timeout.Elapsed.Ticks * total / localDone);
-                                if (estimatedTotalTime > context.Properties.SolverTimeout)
-                                    throw new TimeoutException($"Solver total time estimation {estimatedTotalTime} exceeds limit {context.Properties.SolverTimeout}");
-                            }
-                            
-                            exceptionDispatchInfo?.Throw();
-
-                            var state = new MutableState(task.Problem.Matrix);
-                            var queue = new Queue<ICommand>(commands);
-                            while (queue.Any())
-                            {
-                                state.Tick(queue);
-                            }
-                            state.EnsureIsFinal();
-
-                            result.SecondsSpent = (int)timer.Elapsed.TotalSeconds;
-                            result.EnergySpent = state.Energy;
-                            result.EnergyHistory = state.EnergyHistory;
-                            result.Solution = CommandSerializer.Save(commands.ToArray()).SerializeSolutionToString();
-                            result.IsSuccess = true;
-                        }
-                        catch (Exception e)
+                                }
+                                catch (Exception exception)
+                                {
+                                    exceptionDispatchInfo = ExceptionDispatchInfo.Capture(exception);
+                                    started.Set();
+                                }
+                            });
+                        runThread.Start();
+                        if (!started.WaitOne(context.Properties.SolverStartTimeout))
                         {
-                            context.Log.Warn($"Unhandled exception in solver for {task.Problem.FileName}");
-
-                            result.IsSuccess = false;
-                            result.ExceptionInfo = $"{e.Message}\n{e.StackTrace}";
+                            runThread.Abort();
+                            runThread.Join();
+                            throw new TimeoutException("Solve start timeout expired");
                         }
 
-                        context.Log.Info($"Task {result.TaskName} " +
-                                         $"with solver {result.SolverName} " +
-                                         $"at {result.RunningHostName}: " +
-                                         $"completed in {result.SecondsSpent}s");
+                        while (!runThread.Join(context.Properties.SolverTimeoutMeasureInterval))
+                        {
+                            var localDone = Interlocked.CompareExchange(ref done, 0, 0);
+                            if (localDone == 0)
+                                throw new TimeoutException("Solver didn't fill any cells");
 
-                        completeTasksCounter++;
-                        context.Log.Info($"Tasks complete: {completeTasksCounter} of {selectedTasks.Length} for this worker");
+                            var estimatedTotalTime = TimeSpan.FromTicks(timeout.Elapsed.Ticks * total / localDone);
+                            if (estimatedTotalTime > context.Properties.SolverTimeout)
+                                throw new TimeoutException($"Solver total time estimation {estimatedTotalTime} exceeds limit {context.Properties.SolverTimeout}");
+                        }
 
-                        client.IndexDocument(result);
-                    });
+                        exceptionDispatchInfo?.Throw();
 
-                context.Log.Info("Sleeping forever, all tasks done");
-                Thread.Sleep(int.MaxValue);
-            }
+                        var state = new MutableState(task.Problem.Matrix);
+                        var queue = new Queue<ICommand>(commands);
+                        while (queue.Any())
+                        {
+                            state.Tick(queue);
+                        }
+                        state.EnsureIsFinal();
+
+                        result.SecondsSpent = (int)timer.Elapsed.TotalSeconds;
+                        result.EnergySpent = state.Energy;
+                        result.EnergyHistory = state.EnergyHistory;
+                        result.Solution = CommandSerializer.Save(commands.ToArray()).SerializeSolutionToString();
+                        result.IsSuccess = true;
+                    }
+                    catch (Exception e)
+                    {
+                        context.Log.Warn($"Unhandled exception in solver for {task.Problem.FileName}");
+
+                        result.IsSuccess = false;
+                        result.ExceptionInfo = $"{e.Message}\n{e.StackTrace}";
+                    }
+
+                    context.Log.Info($"Task {result.TaskName} " +
+                                     $"with solver {result.SolverName} " +
+                                     $"at {result.RunningHostName}: " +
+                                     $"completed in {result.SecondsSpent}s");
+
+                    completeTasksCounter++;
+                    context.Log.Info($"Tasks complete: {completeTasksCounter} of {selectedTasks.Length} for this worker");
+
+                    client.IndexDocument(result);
+                });
+
+            context.Log.Info("Sleeping forever, all tasks done");
+            Thread.Sleep(int.MaxValue);
         }
     }
 }
