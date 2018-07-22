b4ebc5959668998343330d8eac9e3eb4a20d7496
spaceorc <spaceorc@skbkontur.ru>
2018-07-22 22:46:11 +0500

    Fix smth

---
+++ lib/Strategies/Features/DeriveOne.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+
+using lib.Commands;
+using lib.Models;
+using lib.Primitives;
+using lib.Strategies.Features.Async;
+using lib.Utils;
+
+namespace lib.Strategies.Features
+{
+    public class DeriveOne : SimpleSingleBotStrategyBase
+    {
+        public DeriveOne(DeluxeState state, Bot bot)
+            : base(state, bot)
+        {
+        }
+
+        public Bot DerivedBot { get; private set; }
+
+        protected override async StrategyTask<bool> Run()
+        {
+            if (!bot.Seeds.Any())
+                return false;
+
+            var to = NearPosition();
+            if (to == null)
+                return false;
+
+            var newBid = bot.Seeds[0];
+            await Do(new Fission(new NearDifference(to - bot.Position), bot.Seeds.Count - 1));
+
+            DerivedBot = state.Bots.Single(b => b.Bid == newBid);
+            return true;
+        }
+
+        private Vec NearPosition()
+        {
+            return bot.Position
+                      .GetNears()
+                      .Where(n => n.IsInCuboid(state.Matrix.R))
+                      .FirstOrDefault(n => !state.VolatileCells.ContainsKey(n));
+        }
+    }
+}
--- lib/Strategies/Features/Split.cs
+++ lib/Strategies/Features/Split.cs
@@ -1,45 +1,37 @@
+using System;
 using System.Collections.Generic;
-using System.Linq;
 
-using lib.Commands;
 using lib.Models;
-using lib.Primitives;
 using lib.Strategies.Features.Async;
-using lib.Utils;
 
 namespace lib.Strategies.Features
 {
     public class Split : SimpleSingleBotStrategyBase
     {
-        public Split(DeluxeState state, Bot bot)
+        private readonly int count;
+
+        public Split(DeluxeState state, Bot bot, int count = int.MaxValue)
             : base(state, bot)
         {
+            this.count = Math.Min(count, bot.Seeds.Count + 1);
         }
+
+        public List<Bot> Bots { get; } = new List<Bot>();
 
         protected override async StrategyTask<bool> Run()
         {
-            if (bot.Seeds.Count == 1)
+            Bots.Add(bot);
+            var generator = bot;
+            for (int i = 0; i < count - 1; i++)
             {
-                return false;
+                var deriveOne = new DeriveOne(state, generator);
+                if (!await deriveOne)
+                    return false;
+
+                generator = deriveOne.DerivedBot;
+                Bots.Add(deriveOne.DerivedBot);
             }
-
-            var to = NearPosition();
-
-            if (to != null)
-            {
-                await Do(new Fission(new NearDifference(to - bot.Position), bot.Seeds[(bot.Seeds.Count + 1) / 2]));
-                return true;
-            }
-
-            return false;
-        }
-
-        private Vec NearPosition()
-        {
-            return bot.Position
-                      .GetNears()
-                      .Where(n => n.IsInCuboid(state.Matrix.R))
-                      .FirstOrDefault(n => !state.VolatileCells.ContainsKey(n));
+            return true;
         }
     }
 }
