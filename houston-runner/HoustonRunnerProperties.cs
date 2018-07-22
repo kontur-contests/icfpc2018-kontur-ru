5a2d240945ebef84d3bb3d252c26747d872b3d0f
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2018-07-22 18:44:44 +0500

    Increase solver start timeout to accomodate non-lazy inverted solvers

--- houston-runner/HoustonRunnerProperties.cs
+++ houston-runner/HoustonRunnerProperties.cs
@@ -4,7 +4,7 @@ namespace houston
 {
     public class HoustonRunnerProperties
     {
-        public TimeSpan SolverStartTimeout = TimeSpan.FromMinutes(10);
+        public TimeSpan SolverStartTimeout = TimeSpan.FromMinutes(30);
         public TimeSpan SolverTimeoutMeasureInterval = TimeSpan.FromMinutes(1);
         public TimeSpan SolverTimeout = TimeSpan.FromMinutes(60);
     }
